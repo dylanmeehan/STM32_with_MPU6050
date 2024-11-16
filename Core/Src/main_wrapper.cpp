2f402ffd682f71352e3529e2e7b64f34fe79017e
dylanmeehan <dylanmeehan@gmail.com>
2024-11-16 15:34:14 -0800

    blinking led using timer 1

--- Core/Src/main_wrapper.cpp
+++ Core/Src/main_wrapper.cpp
@@ -5,6 +5,21 @@ extern "C" {
 
 static Mpu6050 mpu6050;
 
+extern TIM_HandleTypeDef htim1;
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
+  if(htim->Instance == TIM1){
+    // when timer rolls over (arr), set green led on
+    HAL_GPIO_WritePin(G_LED_GPIO_Port, G_LED_Pin, GPIO_PIN_SET);
+  }
+}
+
+void HAL_TIM_OC_DelayElapsedCallback(TIM_HandleTypeDef *htim){
+  if(htim->Instance == TIM1){
+    // when timer1 hits OC value, set green led off
+    HAL_GPIO_WritePin(G_LED_GPIO_Port, G_LED_Pin, GPIO_PIN_RESET);
+  }
+}
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
   switch(GPIO_Pin){
@@ -18,19 +33,15 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
 }
 
 void Main_Wrapper_Init(){
-  mpu6050.Init(GyroScale::GYRO_2000DPS, AccelScale::ACCEL_16G);
+  HAL_TIM_Base_Start_IT(&htim1);
+  HAL_TIM_OC_Start_IT(&htim1, TIM_CHANNEL_1);  
+
+  mpu6050.Init(GyroScale::GYRO_250DPS, AccelScale::ACCEL_2G);
 }
 
 void Main_Wrapper_Loop(){
   
   mpu6050.ReadIfReady();
-
-  // LED blink
-  HAL_GPIO_WritePin(G_LED_GPIO_Port, G_LED_Pin, GPIO_PIN_SET);
-  HAL_Delay(200);
-  HAL_GPIO_WritePin(G_LED_GPIO_Port, G_LED_Pin, GPIO_PIN_RESET);
-  HAL_Delay(300);
-
 
 }
 
